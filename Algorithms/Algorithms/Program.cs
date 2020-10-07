79ab76a5cb3810113caf4111ee46d27281982955
Frans911 <212281972@tut4life.ac.za>
2020-10-07 13:30:44 +0200

    Shuffle

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -6,11 +6,19 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            int[] nums = { 1, 2, 3, 4 };
-            RunningSum.runningSum(nums);
+            //int[] nums = { 1, 2, 3, 4 };
+            //RunningSum.runningSum(nums);
+            //foreach (var item in nums)
+            //{
+            //    Console.Write("{0} ", item);
+            //}
+
+            int[] nums = { 1, 1, 2, 2 };
+            int n = 2;
+            Shuffle.shuffle(nums, n);
             foreach (var item in nums)
             {
-                Console.Write("{0} ", item);
+                Console.Write("{0},", item);
             }
 
             Console.WriteLine("\nPress any key to continue..."); 
---
+++ Algorithms/Algorithms/Shuffle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    class Shuffle
+    {
+        public static int[] shuffle(int[] numbers, int n)
+        {
+            var value1 = new int[n];
+            var value2 = new int[n];
+
+            if ((n >= 1 && n <= 500) && numbers.Length == 2 * n)
+            {
+                for (var i = 0; i < n; i++)
+                {
+                    if (numbers[i] >= 1 && numbers[i] <= Math.Pow(10, 3))
+                    {
+                        value1[i] = numbers[i];
+                        value2[i] = numbers[n + i];
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your values must range from 1 to 1000");
+                        break;
+                    }
+                }
+                var x = 0;
+                var y = 0;
+                for (var i = 0; i < 2 * n; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        numbers[i] = value1[x];
+                        x++;
+                    }
+                    else
+                    {
+                        numbers[i] = value2[y];
+                        y++;
+                    }
+                }
+            }
+            return numbers;
+        }
+    }
+}
