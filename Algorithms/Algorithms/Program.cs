fca539d5f2e7287ec2e6e68411764e1bbbb2cf08
Frans911 <212281972@tut4life.ac.za>
2020-10-07 12:09:07 +0200

    Running Sum Function

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -6,7 +6,14 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
+            int[] nums = { 1, 2, 3, 4 };
+            RunningSum.runningSum(nums);
+            foreach (var item in nums)
+            {
+                Console.Write("{0} ", item);
+            }
 
+            Console.WriteLine("\nPress any key to continue...");
             Console.ReadKey();
         }
     }
---
+++ Algorithms/Algorithms/RunningSum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    class RunningSum
+    {
+        public static int[] runningSum(int[] numbers)
+        { 
+            for (int x = 1; x < numbers.Length; x++)
+            { 
+                numbers[x] += numbers[x - 1];
+            }
+            return numbers;
+        }
+    }
+}
