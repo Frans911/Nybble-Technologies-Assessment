94f27b448acc2cb5ee45b401188902dd9618fd58
Frans911 <212281972@tut4life.ac.za>
2020-10-07 13:12:34 +0200

    Running Sum Function With Constraints

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -6,14 +6,14 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            int[] nums = { 1, 2, 3, 4 };
+            int[] nums = { 1, 2000000, 3, 4 };
             RunningSum.runningSum(nums);
             foreach (var item in nums)
             {
                 Console.Write("{0} ", item);
             }
 
-            Console.WriteLine("\nPress any key to continue...");
+            Console.WriteLine("\nPress any key to continue..."); 
             Console.ReadKey();
         }
     }
--- Algorithms/Algorithms/RunningSum.cs
+++ Algorithms/Algorithms/RunningSum.cs
@@ -7,11 +7,27 @@ namespace Algorithms
     class RunningSum
     {
         public static int[] runningSum(int[] numbers)
-        { 
-            for (int x = 1; x < numbers.Length; x++)
-            { 
-                numbers[x] += numbers[x - 1];
+        {
+            if ((numbers.Length >= 1 && numbers.Length <= 1000))
+            {
+                for (int x = 1; x < numbers.Length; x++)
+                {
+                    if (numbers[x] >= Math.Pow(-10,6) * -1 && numbers[x] <= Math.Pow(10,6))
+                    {
+                        numbers[x] += numbers[x - 1];
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your values must range from -1000000 to 1000000");
+                        break;
+                    }
+                }
             }
+            else 
+            {
+                Console.WriteLine("Your list is too large");
+            }
+
             return numbers;
         }
     }
