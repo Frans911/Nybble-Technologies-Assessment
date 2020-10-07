c73d7ca54304f4dfc0ff34f2704c095b2d67b908
Frans911 <212281972@tut4life.ac.za>
2020-10-07 15:26:26 +0200

    Substring Function With Constraints

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -13,17 +13,20 @@ namespace Algorithms
             //    Console.Write("{0} ", item);
             //}
 
-            int[] nums = { 1, 1, 2, 2 };
-            int n = 7;
-            Shuffle.shuffle(nums, n);
-            foreach (var item in nums)
-            {
-                Console.Write("{0},", item);
-            }
+            //int[] nums = { 1, 1, 2, 2 };
+            //int n = 7;
+            //Shuffle.shuffle(nums, n);
+            //foreach (var item in nums)
+            //{
+            //    Console.Write("{0},", item);
+            //}
 
             //string J = "aA", S = "aAAbbbb";
             //var answer = JewelsInStones.numJewelsInStones(J, S);
             //Console.WriteLine(answer);
+
+            string s = "abcabc";
+            Console.WriteLine(Substrings.numberOfSubstrings(s));
 
             Console.WriteLine("\nPress any key to continue..."); 
             Console.ReadKey();
---
+++ Algorithms/Algorithms/Substrings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    class Substrings
+    {
+        public static int numberOfSubstrings(string s)
+        {
+            int answer = 0;
+            int[] digits = new int[3];
+            if ((s.Length >= 3 && s.Length <= 5 * Math.Pow(10, 4)) && (s.Contains('a') || s.Contains('b') || s.Contains('c')))
+            {
+                for (int i = 0, j = 0; i < s.Length; i++)
+                {
+                    digits[s[i] - 'a']++;
+                    while (digits[0] > 0 && digits[1] > 0 && digits[2] > 0)
+                    {
+                        answer += s.Length - i;
+                        digits[s[j] - 'a']--;
+                        j++;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your s must have more than 3 letters and less than 5000 letters and s consist of only a, b and c");
+            }
+            return answer;
+        }
+    }
+}
